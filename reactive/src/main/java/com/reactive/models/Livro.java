8f6148a7483241d1deea6e291a542f81aa9f2d08
Alex Francisco <alexfsdn@gmail.com>
2021-09-06 16:19:31 -0300

    webflux reactive application development with h2

--- reactive/src/main/java/com/reactive/models/Livro.java
+++ reactive/src/main/java/com/reactive/models/Livro.java
@@ -10,9 +10,8 @@ import java.io.Serializable;
 public class Livro implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @org.springframework.data.annotation.Id
+    @Id
     private Long id;
     private String nome;
     private boolean capaDura;
